3da18262e6746cc6cc3ee938ed2fe24c5ae3542f
shaymuel <97071744+shaymuel@users.noreply.github.com>
2022-04-29 19:00:58 -0400

    add rest inputs
    added rest inputs

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,24 @@
 const routes = require('express').Router();
 const connect = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 routes.get('/', (req, res) => {
-
-  const dotenv = require('dotenv');
-  dotenv.config();
-
-
-  connect.getCollection("contacts").find().toArray((err, result) => {
-    if (err) throw err;
-    res.json(result);
-    console.log("Contact Query Is Successful")
+  const results = connect.getCollection("contacts").find();
+  results.toArray().then((documents) => {
+    res.status(200).json(documents);
+    console.log('Returned All Contacts');
   });
 });
-//res.send('Contacts')
+
+routes.get('/:id', (req, res) => {
+  const contactId = new ObjectId(req.params.id)
+  const results = connect.getCollection("contacts").find({_id: contactId});
+  results.toArray().then((documents) => {
+    res.status(200).json(documents[0]);
+    console.log('Returned One Contact');
+  });
+});
+
+
 
 module.exports = routes;
