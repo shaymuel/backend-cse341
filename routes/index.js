bce1e5aedb8ba83f160c24b974400095998fa4c6
shaymuel <97071744+shaymuel@users.noreply.github.com>
2022-04-29 17:25:47 -0400

    adding routes
    adding routes for contacts and home

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
-    res.send('Konnor Mueller')
-  })
+  res.send('Konnor Mueller')
+})
+
+routes.use('/home', require('./home'))
+routes.use('/contacts', require('./contacts'))
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -9,18 +9,3 @@ app.use('/', require('./routes'))
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(process.env.MONGODB_URI, function (err, db) {
-  if (err) throw err;
-  var dbo = db.db("MongoDB");
-  dbo.collection("contacts").find().toArray(function (err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close()
-
-  });
-});
